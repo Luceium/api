c4236ac2a86b4e1ef3eb0417c4aded4039f5319f
Mau <79354255+Luceium@users.noreply.github.com>
2023-11-07 15:48:11 -0800

    adds makeFile method to python script

--- contact/encode.py
+++ contact/encode.py
@@ -5,13 +5,20 @@ import sys
 
 
 def encode(file):
-    with open(file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
+    with open(file, "rb") as target:
+        encoded_string = base64.b64encode(target.read())
         return encoded_string.decode("utf-8")
+
+
+def makeFile(file, encoded_string):
+    with open(file, "w") as target:
+        target.write(encoded_string)
 
 
 if (len(sys.argv) != 2):
     print("Usage: python3 encode.py <file-path>")
     exit(1)
 
-print(encode(sys.argv[1]))
+encoded_string = encode(sys.argv[1])
+# -4 removes 3 letter file extension and 1 period
+makeFile(sys.argv[1][:-4] + ".base64", encoded_string)
