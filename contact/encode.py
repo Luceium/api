21bd4d6b1f71f85a2a66e408de7bc555667faa9c
Mau <79354255+Luceium@users.noreply.github.com>
2023-11-07 17:25:44 -0800

    revert changes and let workflow handle file writing

--- contact/encode.py
+++ contact/encode.py
@@ -10,15 +10,8 @@ def encode(file):
         return encoded_string.decode("utf-8")
 
 
-def makeFile(file, encoded_string):
-    with open(file, "w") as target:
-        target.write(encoded_string)
-
-
 if (len(sys.argv) != 2):
     print("Usage: python3 encode.py <file-path>")
     exit(1)
 
-encoded_string = encode(sys.argv[1])
-# -4 removes 3 letter file extension and 1 period
-makeFile(sys.argv[1][:-4] + ".base64", encoded_string)
+print(encode(sys.argv[1]))
